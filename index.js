73ee6ca98e9938a4c2275f5dae83671493483de4
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 07:20:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays () {
-  returns "Happy holidays!"
+  return "Happy holidays!"
 }
 function happyHolidaysTo(string){
-  returns `Happy holidays, ${string}`
+  return `Happy holidays, ${string}`
 }
 function happyHolidayTo(holiday, name) {
-  returns `Happy ${holiday}, ${name}`
+  return `Happy ${holiday}, ${name}`
 }
 function holidayCountdown(holiday, days) {
   return 'It\'s ${days} days until ${holiday}!'
