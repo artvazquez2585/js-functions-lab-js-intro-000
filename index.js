ab93ef673ca9248d549384769df92b929dc6e9cf
ARTURO VAZQUEZ CANEDO <art_vazquez_2585@yahoo.com>
2019-11-29 07:20:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 function happyHolidays () {
   returns "Happy holidays!"
 }
+function happyHolidaysTo(string){
+  returns `Happy holidays, ${string}`
+}
+function happyHolidayTo(holiday, name) {
+  returns `Happy ${holiday}, ${name}`
+}
+function holidayCountdown(holiday, days) {
+  return 'It\'s ${days} days until ${holiday}!'
+}
